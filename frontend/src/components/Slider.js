e76d2a6730931ddbfef45b686713ea8a88c8f726
ydogdu <ydogdu@infina.com.tr>
2023-05-19 00:19:10 +0300

    Fixed empty span texts for range values at the beginning.

--- frontend/src/components/Slider.js
+++ frontend/src/components/Slider.js
@@ -2,20 +2,21 @@ import React, { useEffect, useState } from 'react';
 import map from './assets/map.png';
 
 function Slider() {
-  const [sliderValue, setSliderValue] = useState(9);
-  const [fuelValue, setFuelValue] = useState(3);
-  const [yearValue, setYearValue] = useState(21);
-  const [warrantyValue, setWarrantyValue] = useState(5);
-  const [brandValue, setBrandValue] = useState(30);
-  const [locationValue, setLocationValue] = useState(6);
+  var defaultValue = 2011;
+  const [sliderValue, setSliderValue] = useState(7);
+  const [fuelValue, setFuelValue] = useState(1);
+  const [yearValue, setYearValue] = useState(defaultValue);
+  const [warrantyValue, setWarrantyValue] = useState(1);
+  const [brandValue, setBrandValue] = useState(12);
+  const [locationValue, setLocationValue] = useState(3);
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const colorNames = ["Black", "Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet", "White"];  const [sliderText, setSliderText] = useState(colorNames[sliderValue]);
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const fuelNames = ["Gasoline", "Diesel", "LPG"]; const [FuelText, setFuelText] = useState(fuelNames[fuelValue]);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+  // eslint-disable-next-line react-hooks/exhaustive-deps, no-unused-vars
   const yearNames = ["2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"]; const [yaerText, setYearText] = useState(yearNames[yearValue]);
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  const warrantyNames = ["No warranty", "3 months", "6 months", "1 year", "DS warranty"]; const [warrantyText, setWarrantyText] = useState(warrantyNames[warrantyValue]);
+  const warrantyNames = ["No warranty", "3 months", "6 months", "1 year", "DS warranty"]; var [warrantyText, setWarrantyText] = useState(warrantyNames[warrantyValue]);
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const brandNames = ["Alfa Romeo", "Mitsubishi", "MINI", "Mazda", "Cupra", "SUZUKI", "Subaru", "Jeep", "Ssangyong", "Porsche", "Mercedes-Benz", "Honda", "Toyota", "Skoda", "SEAT", "Renault", "Peugeot", "Opel", "Nissan", "Land Rover", "KIA", "HYUNDAI", "Ford", "Fiat", "Dacia", "Citroen", "BMW", "Audi", "Volvo", "Volkswagen"]; const [brandText, setBrandText] = useState(brandNames[brandValue]);
   // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -88,22 +89,29 @@ function Slider() {
     var spans4 = document.getElementById("spans4");
     var spans5 = document.getElementById("spans5");
     var spans6 = document.getElementById("spans6");
-    if (color === "white") {
-      spans.style.color = "white";
-      spans2.style.color = "white";
-      spans3.style.color = "white";
-      spans4.style.color = "white";
-      spans5.style.color = "white";
-      spans6.style.color = "white";
-    }
-    else if (color === "black") {
-      spans.style.color = "black";
-      spans2.style.color = "black";
-      spans3.style.color = "black";
-      spans4.style.color = "black";
-      spans5.style.color = "black";
-      spans6.style.color = "black";
-    }
+    if (typeof(spans) != 'undefined' && spans != null &&
+        typeof(spans2) != 'undefined' && spans2 != null &&
+        typeof(spans3) != 'undefined' && spans3 != null &&
+        typeof(spans4) != 'undefined' && spans4 != null &&
+        typeof(spans5) != 'undefined' && spans5 != null &&
+        typeof(spans6) != 'undefined' && spans6 != null) {
+          if (color === "white") {
+            spans.style.color = "white";
+            spans2.style.color = "white";
+            spans3.style.color = "white";
+            spans4.style.color = "white";
+            spans5.style.color = "white";
+            spans6.style.color = "white";
+          }
+          else if (color === "black") {
+            spans.style.color = "black";
+            spans2.style.color = "black";
+            spans3.style.color = "black";
+            spans4.style.color = "black";
+            spans5.style.color = "black";
+            spans6.style.color = "black";
+          }
+        }
   }
 
   function backgroundChanger() {
@@ -160,26 +168,30 @@ function Slider() {
     }
   }
 
+  const renderSpanText = () => {
+      return <span>Manufacturing Year: {yearValue}</span>;
+  };
+
   return (
     <>
       <div className="wrapper">
         <section className="all-sliders">
           <label className="slider-container first">
-            <span id='spans'>Manufacturing Year: {yaerText}</span>
+            {renderSpanText()}
             <input
               className="slider"
               id="range-slider"
               type="range"
-              min="0"
+              min="2002"
               step="0.01"
-              max="20"
-              defaultValue="9"
+              max="2022"
+              defaultValue={defaultValue}
               onChange={handleSliderChangeThree}
             />
           </label>
 
           <label className="slider-container second">
-            <span id='spans2'>Warranty Period: {warrantyText}</span>
+            <span>Warranty Period: {warrantyText}</span>
             <input
               className="slider"
               id="range-slider2"
@@ -242,13 +254,13 @@ function Slider() {
               type="range"
               min="0"
               step="0.01"
-              max="20"
+              max="29"
               defaultValue="13"
               onChange={handleSliderChangeFive}
             />
           </label>
 
-          <img id="map" src={map} alt="Map" />;
+          <img id="map" src={map} alt="Map" />
         </section>
       </div>
     </>
